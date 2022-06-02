1c8086ac3bbfd55b36030fc26a90ceef23a3ed5f
mariobourgeois <mariobourgeois97.200@gmail.com>
2022-06-02 15:44:37 -0400

    ajout des section présentation et imfpa

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,14 +2,26 @@
 @section('content')
 
 {{-- Début partie présentation--}}
-<div class="presentation">
-  <h2>Présentation</h2>
-</div>
+<section class="presentation">
+  <h2>PRÉSENTATION</h2>
+</section>
 {{-- Fin partie présentation --}}
 
-
+<br>
 
 {{-- Début partie imfpa --}}
-    <h2>test</h2>
+
+<section class="imfpa">
+
+    <h2>IMFPA</h2>
+     <span class="titre"> Horaires:</span> <br>
+      Lundi, Mardi et Jeudi : 7h30-12h30 / 13h30-16h30 <br>
+      Mercredi et Vendredi : 7h30 – 13h00 <br>
+
+    <a href="https://www.linkedin.com/company/imfpa/"><i class="reseauximfpa fa-brands fa-linkedin"></i> </a>
+    <a href="https://fr-fr.facebook.com/imfpa972/"><i class="reseauximfpa-facebook fa-brands fa-facebook"></i></a>
+
+</section>
+
 {{-- Fin partie imfpa --}}
   @endsection
