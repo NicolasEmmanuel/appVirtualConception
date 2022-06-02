f4898dc95dab684644aa090891f72d3c85e6a8e4
mariobourgeois <mariobourgeois97.200@gmail.com>
2022-06-02 14:39:49 -0400

    modif css

--- public/js/app.js
+++ public/js/app.js
@@ -22251,6 +22251,19 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/css/contact.css":
+/*!***********************************!*\
+  !*** ./resources/css/contact.css ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
 /***/ "./resources/css/app.css":
 /*!*******************************!*\
   !*** ./resources/css/app.css ***!
@@ -22594,6 +22607,7 @@ module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"P
 /******/ 			"/js/app": 0,
 /******/ 			"css/admin": 0,
 /******/ 			"css/app": 0,
+/******/ 			"css/contact": 0,
 /******/ 			"css/style": 0
 /******/ 		};
 /******/ 		
@@ -22644,10 +22658,11 @@ module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"P
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["css/admin","css/app","css/style"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	__webpack_require__.O(undefined, ["css/admin","css/app","css/style"], () => (__webpack_require__("./resources/css/style.css")))
-/******/ 	__webpack_require__.O(undefined, ["css/admin","css/app","css/style"], () => (__webpack_require__("./resources/css/admin.css")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/admin","css/app","css/style"], () => (__webpack_require__("./resources/css/app.css")))
+/******/ 	__webpack_require__.O(undefined, ["css/admin","css/app","css/contact","css/style"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	__webpack_require__.O(undefined, ["css/admin","css/app","css/contact","css/style"], () => (__webpack_require__("./resources/css/style.css")))
+/******/ 	__webpack_require__.O(undefined, ["css/admin","css/app","css/contact","css/style"], () => (__webpack_require__("./resources/css/admin.css")))
+/******/ 	__webpack_require__.O(undefined, ["css/admin","css/app","css/contact","css/style"], () => (__webpack_require__("./resources/css/contact.css")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/admin","css/app","css/contact","css/style"], () => (__webpack_require__("./resources/css/app.css")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
